335f0f20c1587606c0d8ae21d8c3817ec1705ed0
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-28 21:51:44 -0600

    controller

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -10,16 +10,15 @@ const tokenExpirationMiddleware = require('../middlewares/verifyTokenExpiration'
 const categoryController = require('../controllers/categoryController');
 
 // Rutas CRUD para Categorías
-
-//Obtiene todas las categorías.
-router.get('/', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.getAllCategories);
-//Obtiene una categoría por su ID.
-router.get('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.getCategoryById);
 //	Crea una nueva categoría.
 router.post('/', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.createCategory);
-//Actualiza una categoría por ID.
-router.put('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.updateCategory);
+//Obtiene todas las categorías
+router.get('/', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.getAllCategories);
 //Elimina una categoría de la base de datos.
 router.delete('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.deleteCategory);
+//Obtiene una categoría por su ID.
+router.get('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.getCategoryById);
+//Actualiza una categoría por ID.
+router.put('/:id', authMiddleware, tokenExpirationMiddleware.verifyTokenExpiration, roleMiddleware(['administrador']), categoryController.updateCategory);
 
 module.exports = router;
