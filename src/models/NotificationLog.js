89fd23f801ad38f8c23cfdffd3162d8e9b120d7f
LUISGERARDO11 <luis34julieta@gmail.com>
2025-03-08 22:51:21 -0600

    segunda prueba para el cambio de enfoque de envio de email

--- src/models/NotificationLog.js
+++ src/models/NotificationLog.js
@@ -10,7 +10,7 @@ const NotificationLog = sequelize.define('NotificationLog', {
   },
   user_id: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    allowNull: true,
     references: {
       model: 'users',
       key: 'user_id'
