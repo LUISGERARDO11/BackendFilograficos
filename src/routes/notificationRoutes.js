257ecb35e7fac190b734700a65f88addb148eb19
LUISGERARDO11 <luis34julieta@gmail.com>
2025-03-10 22:06:06 -0600

    actualizacion de notification router para poder remover una suscripcion

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -4,5 +4,6 @@ const notificationController = require('../controllers/notificationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/subscribe', authMiddleware, notificationController.subscribeToPush);
+router.delete('/unsubscribe', authMiddleware, notificationController.unsubscribeFromPush);
 
 module.exports = router;
