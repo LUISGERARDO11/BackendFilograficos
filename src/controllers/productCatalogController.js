ddf7ae80747dbc5ddda21aac40f49ef31e9ada5a
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-03-23 19:05:38 -0600

    modified:   src/controllers/productCatalogController.js 	modified:   src/controllers/productPriceController.js 	modified:   src/middlewares/productValidation.js 	modified:   src/models/Product.js

--- src/controllers/productCatalogController.js
+++ src/controllers/productCatalogController.js
@@ -10,8 +10,8 @@ const validateGetAllProducts = [
   query('category_id').optional().isInt({ min: 1 }).withMessage('El ID de la categoría debe ser un entero positivo'),
   query('product_type')
     .optional()
-    .isIn(['Existencia', 'semi_personalizado', 'personalizado'])
-    .withMessage('El tipo de producto debe ser "Existencia", "semi_personalizado" o "personalizado"'),
+    .isIn(['Existencia', 'Personalizado']) // Eliminamos 'semi_personalizado'
+    .withMessage('El tipo de producto debe ser "Existencia" o "Personalizado"'), // Actualizamos el mensaje
   query('page').optional().isInt({ min: 1 }).withMessage('La página debe ser un entero positivo'),
   query('pageSize').optional().isInt({ min: 1 }).withMessage('El tamaño de página debe ser un entero positivo'),
   query('sort')
