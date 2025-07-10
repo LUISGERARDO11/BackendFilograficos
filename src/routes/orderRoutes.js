fd6b5a13eeafcc37ca2fb0b7c7f20f31c705917e
Hailie <htapiahailie@gmail.com>
2025-07-09 23:53:08 -0600

    modified:   src/routes/orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,24 +5,35 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const tokenExpirationMiddleware = require('../middlewares/verifyTokenExpiration');
 
 // Crear una orden
-router.post('/create', 
-  authMiddleware, 
-  tokenExpirationMiddleware.verifyTokenExpiration, 
+router.post('/create',
+  authMiddleware,
+  tokenExpirationMiddleware.verifyTokenExpiration,
   orderController.createOrder
 );
 
 // Obtener detalles de una orden por ID
-router.get('/:id', 
-  authMiddleware, 
-  tokenExpirationMiddleware.verifyTokenExpiration, 
+router.get('/:id',
+  authMiddleware,
+  tokenExpirationMiddleware.verifyTokenExpiration,
   orderController.getOrderById
 );
 
 // Obtener todas las órdenes del usuario con búsqueda y filtro
-router.get('/', 
-  authMiddleware, 
-  tokenExpirationMiddleware.verifyTokenExpiration, 
+router.get('/',
+  authMiddleware,
+  tokenExpirationMiddleware.verifyTokenExpiration,
   orderController.getOrders
 );
-
+//devolver las opciones activas
+router.get('/shipping-options',
+  authMiddleware,
+  tokenExpirationMiddleware.verifyTokenExpiration,
+  orderController.getShippingOptions
+);
+//devolver las opciones activas
+router.get('/delivery',
+  authMiddleware,
+  tokenExpirationMiddleware.verifyTokenExpiration,
+  orderController.getDeliveryPoints
+);
 module.exports = router;
