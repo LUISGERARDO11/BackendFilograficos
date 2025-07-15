359de47f7fa0637107eef21cfc76d96b67120815
Hailie <htapiahailie@gmail.com>
2025-07-14 21:35:14 -0600

    por milesimavez 	modified:   src/controllers/paymentController.js modified:   src/routes/paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,13 +1,8 @@
+// routes/paymentRoutes.js
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const tokenExpirationMiddleware = require('../middlewares/verifyTokenExpiration');
 
-router.post('/webhook',
-    authMiddleware,
-    tokenExpirationMiddleware.verifyTokenExpiration,
-    paymentController.handleMercadoPagoWebhook
-);
+router.post('/webhook', paymentController.handleMercadoPagoWebhook);
 
 module.exports = router;
