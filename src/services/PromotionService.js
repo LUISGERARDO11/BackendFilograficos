39836b3a6a80f28b166c98667aa4d7d155858f52
HailieTapia <htapiahailie@gmail.com>
2025-08-03 14:20:49 -0600

    modified:   src/services/PromotionService.js

--- src/services/PromotionService.js
+++ src/services/PromotionService.js
@@ -418,6 +418,14 @@ class PromotionService {
         status: 'active'
       }, { transaction });
     }
+    // Validación manual (recomendada)
+    if (applies_to === 'cluster') {
+      const clusterExists = await ClientCluster.findOne({
+        where: { cluster: cluster_id },
+        transaction
+      });
+      if (!clusterExists) throw new Error(`El cluster ${cluster_id} no existe`);
+    }
 
     return await this.getPromotionById(promotion.promotion_id, transaction);
   }
